0c1b4adfd399e4b05c211735a65455f80504d00b
Astewart1510 <61209863+Astewart1510@users.noreply.github.com>
2021-10-27 01:16:12 +0200

    Feat: api call for node graph (#14)

--- dashboard_analytics/views.py
+++ dashboard_analytics/views.py
@@ -7,13 +7,14 @@ from rest_framework.parsers import JSONParser
 from rest_framework import status
 from django.http import HttpResponse
 from django.core import serializers
+from django.db.models import F
 
 from dashboard_analytics.models import AccountType, InstrumentType, Account, Transaction
 from dashboard_analytics.serializers import AccountTypeSerializer, InstrumentTypeSerializer, AccountSerializer, TransactionSerializer
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET'])
 def account_list(request):
     accounts = Account.objects.all()
 
@@ -21,12 +22,21 @@ def account_list(request):
         accounts_serializer = AccountSerializer(accounts, many=True)
         return JsonResponse(accounts_serializer.data, safe=False)
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET'])
 def node_transactions(request):
-    accounts = Account.objects.all()
+    transaction_node = Transaction.objects.select_related('Sender__AccountTypeID', 'Receiver__AccountTypeID', "InstrumentTypeID")
+    node_data = transaction_node.values(
+        id= F("TransactionID"), 
+        amount=F("Amount"),
+        sender=F("Sender"),
+        receiver=F("Receiver"),
+        sender_balance=F("Sender__Balance"),
+        receiver_balance=F("Receiver__Balance"),
+        sender_type=F("Sender__AccountTypeID__Type"),
+        receiver_type=F("Receiver__AccountTypeID__Type"), 
+        instrument_type=F( "InstrumentTypeID__Type"))
     if request.method == 'GET':
-        accounts_serializer = AccountSerializer(accounts, many=True)
-        return JsonResponse(accounts_serializer.data, safe=False)
+        return JsonResponse(list(node_data), safe=False)
 
 
 @api_view(['GET'])
